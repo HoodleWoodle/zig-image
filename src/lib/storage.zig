d635ba4ed4ea42e44046a3b3694360f2c8dcd1e3
hoodlewoodle@web.de <hoodlewoodle@web.de>
2023-08-29 23:24:56 +0200

    add runtime for pixel storage prototype (compiletime)

--- src/lib/storage.zig
+++ src/lib/storage.zig
@@ -231,3 +231,202 @@ pub fn Storage(comptime format: Format) type {
         }
     };
 }
+
+pub const StorageRT = union(Format) {
+    indexed1: Storage(.indexed1),
+    indexed4: Storage(.indexed4),
+    indexed8: Storage(.indexed8),
+    rgba32f: Storage(.rgba32f),
+    rgba32: Storage(.rgba32),
+    bgra32: Storage(.bgra32),
+    argb32: Storage(.argb32),
+    abgr32: Storage(.abgr32),
+    rgb24: Storage(.rgb24),
+    bgr24: Storage(.bgr24),
+    argb4444: Storage(.argb4444),
+    argb1555: Storage(.argb1555),
+    rgb565: Storage(.rgb565),
+    rgb555: Storage(.rgb555),
+    a2r10g10b10: Storage(.a2r10g10b10),
+    a2b10g10r10: Storage(.a2b10g10r10),
+    grayscale1: Storage(.grayscale1),
+    grayscale4: Storage(.grayscale4),
+    grayscale8: Storage(.grayscale8),
+
+    const Self = @This();
+
+    pub fn init(fmt: Format, pixel_count: usize, allocator: Allocator) !Self {
+        return switch (fmt) {
+            .indexed1 => .{ .indexed1 = try Storage(.indexed1).init(pixel_count, allocator) },
+            .indexed4 => .{ .indexed4 = try Storage(.indexed4).init(pixel_count, allocator) },
+            .indexed8 => .{ .indexed8 = try Storage(.indexed8).init(pixel_count, allocator) },
+            .rgba32f => .{ .rgba32f = try Storage(.rgba32f).init(pixel_count, allocator) },
+            .rgba32 => .{ .rgba32 = try Storage(.rgba32).init(pixel_count, allocator) },
+            .bgra32 => .{ .bgra32 = try Storage(.bgra32).init(pixel_count, allocator) },
+            .argb32 => .{ .argb32 = try Storage(.argb32).init(pixel_count, allocator) },
+            .abgr32 => .{ .abgr32 = try Storage(.abgr32).init(pixel_count, allocator) },
+            .rgb24 => .{ .rgb24 = try Storage(.rgb24).init(pixel_count, allocator) },
+            .bgr24 => .{ .bgr24 = try Storage(.bgr24).init(pixel_count, allocator) },
+            .argb4444 => .{ .argb4444 = try Storage(.argb4444).init(pixel_count, allocator) },
+            .argb1555 => .{ .argb1555 = try Storage(.argb1555).init(pixel_count, allocator) },
+            .rgb565 => .{ .rgb565 = try Storage(.rgb565).init(pixel_count, allocator) },
+            .rgb555 => .{ .rgb555 = try Storage(.rgb555).init(pixel_count, allocator) },
+            .a2r10g10b10 => .{ .a2r10g10b10 = try Storage(.a2r10g10b10).init(pixel_count, allocator) },
+            .a2b10g10r10 => .{ .a2b10g10r10 = try Storage(.a2b10g10r10).init(pixel_count, allocator) },
+            .grayscale1 => .{ .grayscale1 = try Storage(.grayscale1).init(pixel_count, allocator) },
+            .grayscale4 => .{ .grayscale4 = try Storage(.grayscale4).init(pixel_count, allocator) },
+            .grayscale8 => .{ .grayscale8 = try Storage(.grayscale8).init(pixel_count, allocator) },
+        };
+    }
+
+    /// conversion may be lossy
+    pub fn from(fmt: Format, from_value: StorageRT, allocator: Allocator) !Self {
+        const pixel_count = from_value.len();
+        var self = try init(fmt, pixel_count, allocator);
+
+        var i: u32 = 0;
+        while (i < pixel_count) : (i += 1) {
+            const intermediate = switch (from_value) {
+                .indexed1 => |data| color.RGBA32F.from(Format.ColorType(.indexed1), try data.at(i)),
+                .indexed4 => |data| color.RGBA32F.from(Format.ColorType(.indexed4), try data.at(i)),
+                .indexed8 => |data| color.RGBA32F.from(Format.ColorType(.indexed8), try data.at(i)),
+                .rgba32f => |data| color.RGBA32F.from(Format.ColorType(.rgba32f), try data.at(i)),
+                .rgba32 => |data| color.RGBA32F.from(Format.ColorType(.rgba32), try data.at(i)),
+                .bgra32 => |data| color.RGBA32F.from(Format.ColorType(.bgra32), try data.at(i)),
+                .argb32 => |data| color.RGBA32F.from(Format.ColorType(.argb32), try data.at(i)),
+                .abgr32 => |data| color.RGBA32F.from(Format.ColorType(.abgr32), try data.at(i)),
+                .rgb24 => |data| color.RGBA32F.from(Format.ColorType(.rgb24), try data.at(i)),
+                .bgr24 => |data| color.RGBA32F.from(Format.ColorType(.bgr24), try data.at(i)),
+                .argb4444 => |data| color.RGBA32F.from(Format.ColorType(.argb4444), try data.at(i)),
+                .argb1555 => |data| color.RGBA32F.from(Format.ColorType(.argb1555), try data.at(i)),
+                .rgb565 => |data| color.RGBA32F.from(Format.ColorType(.rgb565), try data.at(i)),
+                .rgb555 => |data| color.RGBA32F.from(Format.ColorType(.rgb555), try data.at(i)),
+                .a2r10g10b10 => |data| color.RGBA32F.from(Format.ColorType(.a2r10g10b10), try data.at(i)),
+                .a2b10g10r10 => |data| color.RGBA32F.from(Format.ColorType(.a2b10g10r10), try data.at(i)),
+                .grayscale1 => |data| color.RGBA32F.from(Format.ColorType(.grayscale1), try data.at(i)),
+                .grayscale4 => |data| color.RGBA32F.from(Format.ColorType(.grayscale4), try data.at(i)),
+                .grayscale8 => |data| color.RGBA32F.from(Format.ColorType(.grayscale8), try data.at(i)),
+            };
+
+            switch (self) {
+                .indexed1 => |*data| try data.set(i, intermediate),
+                .indexed4 => |*data| try data.set(i, intermediate),
+                .indexed8 => |*data| try data.set(i, intermediate),
+                .rgba32f => |*data| try data.set(i, Format.ColorType(.rgba32f).from(color.RGBA32F, intermediate)),
+                .rgba32 => |*data| try data.set(i, Format.ColorType(.rgba32).from(color.RGBA32F, intermediate)),
+                .bgra32 => |*data| try data.set(i, Format.ColorType(.bgra32).from(color.RGBA32F, intermediate)),
+                .argb32 => |*data| try data.set(i, Format.ColorType(.argb32).from(color.RGBA32F, intermediate)),
+                .abgr32 => |*data| try data.set(i, Format.ColorType(.abgr32).from(color.RGBA32F, intermediate)),
+                .rgb24 => |*data| try data.set(i, Format.ColorType(.rgb24).from(color.RGBA32F, intermediate)),
+                .bgr24 => |*data| try data.set(i, Format.ColorType(.bgr24).from(color.RGBA32F, intermediate)),
+                .argb4444 => |*data| try data.set(i, Format.ColorType(.argb4444).from(color.RGBA32F, intermediate)),
+                .argb1555 => |*data| try data.set(i, Format.ColorType(.argb1555).from(color.RGBA32F, intermediate)),
+                .rgb565 => |*data| try data.set(i, Format.ColorType(.rgb565).from(color.RGBA32F, intermediate)),
+                .rgb555 => |*data| try data.set(i, Format.ColorType(.rgb555).from(color.RGBA32F, intermediate)),
+                .a2r10g10b10 => |*data| try data.set(i, Format.ColorType(.a2r10g10b10).from(color.RGBA32F, intermediate)),
+                .a2b10g10r10 => |*data| try data.set(i, Format.ColorType(.a2b10g10r10).from(color.RGBA32F, intermediate)),
+                .grayscale1 => |*data| try data.set(i, Format.ColorType(.grayscale1).from(color.RGBA32F, intermediate)),
+                .grayscale4 => |*data| try data.set(i, Format.ColorType(.grayscale4).from(color.RGBA32F, intermediate)),
+                .grayscale8 => |*data| try data.set(i, Format.ColorType(.grayscale8).from(color.RGBA32F, intermediate)),
+            }
+        }
+
+        return self;
+    }
+
+    pub fn deinit(self: Self, allocator: Allocator) void {
+        switch (self) {
+            .indexed1 => |data| data.deinit(allocator),
+            .indexed4 => |data| data.deinit(allocator),
+            .indexed8 => |data| data.deinit(allocator),
+            .rgba32f => |data| data.deinit(allocator),
+            .rgba32 => |data| data.deinit(allocator),
+            .bgra32 => |data| data.deinit(allocator),
+            .argb32 => |data| data.deinit(allocator),
+            .abgr32 => |data| data.deinit(allocator),
+            .rgb24 => |data| data.deinit(allocator),
+            .bgr24 => |data| data.deinit(allocator),
+            .argb4444 => |data| data.deinit(allocator),
+            .argb1555 => |data| data.deinit(allocator),
+            .rgb565 => |data| data.deinit(allocator),
+            .rgb555 => |data| data.deinit(allocator),
+            .a2r10g10b10 => |data| data.deinit(allocator),
+            .a2b10g10r10 => |data| data.deinit(allocator),
+            .grayscale1 => |data| data.deinit(allocator),
+            .grayscale4 => |data| data.deinit(allocator),
+            .grayscale8 => |data| data.deinit(allocator),
+        }
+    }
+
+    pub fn format(self: Self) Format {
+        return switch (self) {
+            .indexed1 => .indexed1,
+            .indexed4 => .indexed4,
+            .indexed8 => .indexed8,
+            .rgba32f => .rgba32f,
+            .rgba32 => .rgba32,
+            .bgra32 => .bgra32,
+            .argb32 => .argb32,
+            .abgr32 => .abgr32,
+            .rgb24 => .rgb24,
+            .bgr24 => .bgr24,
+            .argb4444 => .argb4444,
+            .argb1555 => .argb1555,
+            .rgb565 => .rgb565,
+            .rgb555 => .rgb555,
+            .a2r10g10b10 => .a2r10g10b10,
+            .a2b10g10r10 => .a2b10g10r10,
+            .grayscale1 => .grayscale1,
+            .grayscale4 => .grayscale4,
+            .grayscale8 => .grayscale8,
+        };
+    }
+
+    pub fn len(self: Self) usize {
+        return switch (self) {
+            .indexed1 => |data| data.len(),
+            .indexed4 => |data| data.len(),
+            .indexed8 => |data| data.len(),
+            .rgba32f => |data| data.len(),
+            .rgba32 => |data| data.len(),
+            .bgra32 => |data| data.len(),
+            .argb32 => |data| data.len(),
+            .abgr32 => |data| data.len(),
+            .rgb24 => |data| data.len(),
+            .bgr24 => |data| data.len(),
+            .argb4444 => |data| data.len(),
+            .argb1555 => |data| data.len(),
+            .rgb565 => |data| data.len(),
+            .rgb555 => |data| data.len(),
+            .a2r10g10b10 => |data| data.len(),
+            .a2b10g10r10 => |data| data.len(),
+            .grayscale1 => |data| data.len(),
+            .grayscale4 => |data| data.len(),
+            .grayscale8 => |data| data.len(),
+        };
+    }
+
+    pub fn bytes(self: Self) []u8 {
+        return switch (self) {
+            .indexed1 => |data| data.bytes(),
+            .indexed4 => |data| data.bytes(),
+            .indexed8 => |data| data.bytes(),
+            .rgba32f => |data| data.bytes(),
+            .rgba32 => |data| data.bytes(),
+            .bgra32 => |data| data.bytes(),
+            .argb32 => |data| data.bytes(),
+            .abgr32 => |data| data.bytes(),
+            .rgb24 => |data| data.bytes(),
+            .bgr24 => |data| data.bytes(),
+            .argb4444 => |data| data.bytes(),
+            .argb1555 => |data| data.bytes(),
+            .rgb565 => |data| data.bytes(),
+            .rgb555 => |data| data.bytes(),
+            .a2r10g10b10 => |data| data.bytes(),
+            .a2b10g10r10 => |data| data.bytes(),
+            .grayscale1 => |data| data.bytes(),
+            .grayscale4 => |data| data.bytes(),
+            .grayscale8 => |data| data.bytes(),
+        };
+    }
+};
--- src/tests/storage.zig
+++ src/tests/storage.zig
@@ -3,6 +3,7 @@ const color = @import("../lib/color.zig");
 const storage = @import("../lib/storage.zig");
 const StorageFormat = storage.Format;
 const PixelStorage = storage.Storage;
+const PixelStorageRT = storage.StorageRT;
 
 // TODO: TEST: storage
 test "example:" {
@@ -74,5 +75,85 @@ pub fn tmpPrintPixels(s: anytype) void {
     var i: u32 = 0;
     while (i < s.len()) : (i += 1) {
         std.debug.print("{any}\n", .{s.at(i)});
+    }
+}
+
+// TODO: TEST: storageRT
+test "exampleRT:" {
+    std.debug.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n", .{});
+    {
+        var s = try PixelStorageRT.init(StorageFormat.rgb555, 4, std.testing.allocator);
+        defer s.deinit(std.testing.allocator);
+        try s.rgb555.set(0, color.RGB555.init(1, 2, 4));
+        try s.rgb555.set(1, color.RGB555.init(8, 16, 3));
+        try s.rgb555.set(2, color.RGB555.init(31, 12, 14));
+        try s.rgb555.set(3, color.RGB555.init(13, 28, 22));
+
+        tmpPrintBytes(s);
+        tmpPrintPixelsRT(s);
+
+        std.debug.print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n", .{});
+        const s2 = try PixelStorageRT.from(StorageFormat.bgra32, s, std.testing.allocator);
+        defer s2.deinit(std.testing.allocator);
+        tmpPrintPixelsRT(s2);
+
+        std.debug.print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n", .{});
+        const s3 = try PixelStorageRT.from(StorageFormat.indexed4, s2, std.testing.allocator);
+        defer s3.deinit(std.testing.allocator);
+        tmpPrintPixelsRT(s3);
+
+        std.debug.print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n", .{});
+        const s4 = try PixelStorageRT.from(StorageFormat.grayscale1, s3, std.testing.allocator);
+        defer s4.deinit(std.testing.allocator);
+        tmpPrintPixelsRT(s4);
+    }
+    std.debug.print("----------------------------------\n", .{});
+    {
+        var s = try PixelStorageRT.init(StorageFormat.grayscale1, 16, std.testing.allocator);
+        defer s.deinit(std.testing.allocator);
+        var i: u32 = 0;
+        while (i < 16) : (i += 1) {
+            try s.grayscale1.set(i, color.Grayscale1.init(if (i % 2 == 0) 1 else 0));
+        }
+
+        tmpPrintBytes(s);
+        tmpPrintPixelsRT(s);
+
+        std.debug.print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n", .{});
+        const s2 = try PixelStorageRT.from(StorageFormat.rgba32f, s, std.testing.allocator);
+        defer s2.deinit(std.testing.allocator);
+        tmpPrintPixelsRT(s2);
+
+        std.debug.print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n", .{});
+        const s3 = try PixelStorageRT.from(StorageFormat.indexed1, s2, std.testing.allocator);
+        defer s3.deinit(std.testing.allocator);
+        tmpPrintPixelsRT(s3);
+    }
+}
+
+pub fn tmpPrintPixelsRT(s: PixelStorageRT) void {
+    var i: u32 = 0;
+    while (i < s.len()) : (i += 1) {
+        switch (s) {
+            .rgba32f => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .rgba32 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .bgra32 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .argb32 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .abgr32 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .rgb24 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .bgr24 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .argb4444 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .argb1555 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .rgb565 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .rgb555 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .a2r10g10b10 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .a2b10g10r10 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .grayscale1 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .grayscale4 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .grayscale8 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .indexed1 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .indexed4 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+            .indexed8 => |data| std.debug.print("{any}\n", .{data.at(i) catch unreachable}),
+        }
     }
 }
